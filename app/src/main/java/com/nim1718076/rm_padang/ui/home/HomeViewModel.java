57e9e0e2a0cf6e0461bd92c984ab87d0ec8bec40
Crace007 <fatwa.tkj1@gmail.com>
2019-11-21 14:20:51 +0700

    Initial commit

---
+++ app/src/main/java/com/nim1718076/rm_padang/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.nim1718076.rm_padang.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
---
+++ app/src/main/java/com/nim1718076/rm_padang/ui/share/ShareViewModel.java
@@ -0,0 +1,19 @@
+package com.nim1718076.rm_padang.ui.share;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class ShareViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public ShareViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is share fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
